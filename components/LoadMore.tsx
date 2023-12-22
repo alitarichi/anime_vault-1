31f5a34ef99e9d9f4ccf77876e21912553062ff1
Ali Tarichi <tarichiali@gmail.com>
2023-12-22 14:06:31 +0300

    final commit . all working . infinite scroll

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,8 +1,32 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+import AnimeCard from "./AnimeCard";
+
+export type AnimeCard = JSX.Element;
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeCard[]>([]);
+  const [page, setPage] = useState(2);
+
+  useEffect(() => {
+    if (inView) {
+      fetchAnime(page).then((res) => {
+        setData((prevData) => [...prevData, ...res]);
+        setPage((prevPage) => prevPage + 1);
+      });
+    }
+  }, [inView, page]);
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data}
+      </section>
       <section className="flex justify-center items-center w-full">
         <div>
           <Image
@@ -14,6 +38,7 @@ function LoadMore() {
           />
         </div>
       </section>
+      <div ref={ref}></div>
     </>
   );
 }
